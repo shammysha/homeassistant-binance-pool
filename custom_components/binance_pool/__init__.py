4975f4a81f4521df8dd2a1c70fa4f8db440a99bb
Maxim Sharenkov <mcshaman@gmail.com>
2022-08-15 20:44:48 +0300

    translate to translations

--- custom_components/binance_pool/__init__.py
+++ custom_components/binance_pool/__init__.py
@@ -72,7 +72,7 @@ from .client import (
     BinanceRequestException
 )
 
-__version__ = "1.6.19"
+__version__ = "1.6.20"
 REQUIREMENTS = ["python-binance==1.0.10"]
 
 _LOGGER = logging.getLogger(__name__)
