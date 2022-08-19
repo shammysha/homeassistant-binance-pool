7b4cbbc6bad2fdc8afa6b2bce18bbc8caa72a56e
Maxim Sharenkov <mcshaman@gmail.com>
2022-08-19 19:26:27 +0300

    Playing with unload

--- custom_components/binance_pool/__init__.py
+++ custom_components/binance_pool/__init__.py
@@ -290,6 +290,9 @@ async def async_setup_entry(hass, config_entry: ConfigEntry) -> bool:
    
    
 async def async_unload_entry(hass, config_entry: ConfigEntry) -> None:
+    for config_entry in hass.config_entries.async_entries(DOMAIN):
+        _LOGGER.info("Entity is: %s", config_entry.unique_id)
+    
     unload_ops = [
         hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
     ] + [
